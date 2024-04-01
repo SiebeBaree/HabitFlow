5bc7382fa593302217ff27c98aada8a6cf43709b
Siebe Baree <siebe.baree@outlook.com>
2024-04-01 09:19:51 +0200

    Fixed deployment urls

--- src/env.js
+++ src/env.js
@@ -2,10 +2,6 @@ import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
 export const env = createEnv({
-    /**
-     * Specify your server-side environment variables schema here. This way you can ensure the app
-     * isn't built with invalid env vars.
-     */
     server: {
         DATABASE_URL: z
             .string()
@@ -23,21 +19,9 @@ export const env = createEnv({
         AUTH_GOOGLE_ID: z.string(),
         AUTH_GOOGLE_SECRET: z.string(),
         RESEND_API_KEY: z.string(),
+        VERCEL_URL: z.string().url(),
     },
-
-    /**
-     * Specify your client-side environment variables schema here. This way you can ensure the app
-     * isn't built with invalid env vars. To expose them to the client, prefix them with
-     * `NEXT_PUBLIC_`.
-     */
-    client: {
-        NEXT_PUBLIC_APP_URL: z.string().url(),
-    },
-
-    /**
-     * You can't destruct `process.env` as a regular object in the Next.js edge runtimes (e.g.
-     * middlewares) or client-side so we need to destruct manually.
-     */
+    client: {},
     runtimeEnv: {
         DATABASE_URL: process.env.DATABASE_URL,
         NODE_ENV: process.env.NODE_ENV,
@@ -47,16 +31,8 @@ export const env = createEnv({
         AUTH_GOOGLE_ID: process.env.AUTH_GOOGLE_ID,
         AUTH_GOOGLE_SECRET: process.env.AUTH_GOOGLE_SECRET,
         RESEND_API_KEY: process.env.RESEND_API_KEY,
-        NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+        VERCEL_URL: process.env.VERCEL_URL,
     },
-    /**
-     * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
-     * useful for Docker builds.
-     */
     skipValidation: !!process.env.SKIP_ENV_VALIDATION,
-    /**
-     * Makes it so that empty strings are treated as undefined. `SOME_VAR: z.string()` and
-     * `SOME_VAR=''` will throw an error.
-     */
     emptyStringAsUndefined: true,
 });
