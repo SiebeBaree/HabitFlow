c1789ba518511f9a9465d04c5872177062da8d12
Siebe Baree <siebe.baree@outlook.com>
2024-04-02 18:28:48 +0200

    Made small changes to navbar

--- src/components/nav/navbar.tsx
+++ src/components/nav/navbar.tsx
@@ -6,7 +6,6 @@ import {
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import {
-    ChevronDown,
     LayoutDashboardIcon,
     LineChartIcon,
     LogOutIcon,
@@ -14,10 +13,10 @@ import {
     type LucideIcon,
 } from "lucide-react";
 import Link from "next/link";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { auth } from "@/server/auth";
 import { logout } from "@/actions/logout";
-import NavItem from "./nav-item";
+import NavItem from "@/components/nav/nav-item";
+import UserDropdown from "@/components/nav/user-dropdown";
 
 export default async function Navbar() {
     const session = await auth();
@@ -41,18 +40,10 @@ export default async function Navbar() {
             {session ? (
                 <DropdownMenu>
                     <DropdownMenuTrigger className="group outline-none">
-                        <div className="border-highlight flex select-none items-center gap-2 rounded-md bg-secondary px-3 py-2">
-                            <Avatar className="h-6 w-6">
-                                <AvatarImage
-                                    src={session.user.image ?? "/logo.png"}
-                                />
-                                <AvatarFallback>S</AvatarFallback>
-                            </Avatar>
-                            <p className="text-sm font-medium">
-                                {session.user.name}
-                            </p>
-                            <ChevronDown className="h-4 w-4 text-primary transition-all duration-300 ease-in-out group-data-[state=open]:rotate-180" />
-                        </div>
+                        <UserDropdown
+                            image={session.user.image}
+                            name={session.user.name ?? "User"}
+                        />
                     </DropdownMenuTrigger>
                     <DropdownMenuContent className="mr-4 w-48">
                         <DropdownItem
@@ -73,7 +64,7 @@ export default async function Navbar() {
                         <DropdownMenuSeparator />
                         <form action={logout}>
                             <button type="submit">
-                                <DropdownMenuItem className="cursor-pointer text-red-500 transition-all duration-200 ease-in-out hover:bg-white/10">
+                                <DropdownMenuItem className="cursor-pointer text-red-500 transition-all duration-200 ease-in-out hover:bg-black/10">
                                     <LogOutIcon className="mr-2 h-4 w-4" />
                                     <span className="font-medium">Log out</span>
                                 </DropdownMenuItem>
@@ -103,7 +94,7 @@ function DropdownItem({
         <Link href={href ?? "#"}>
             <DropdownMenuItem
                 disabled={disabled}
-                className="cursor-pointer transition-all duration-200 ease-in-out hover:bg-white/10"
+                className="cursor-pointer transition-all duration-200 ease-in-out hover:bg-black/10"
             >
                 {Icon && <Icon className="mr-2 h-4 w-4" />}
                 <span>{name}</span>
---
+++ src/components/nav/user-dropdown.tsx
@@ -0,0 +1,25 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { ChevronDown } from "lucide-react";
+
+type UserDropdownProps = {
+    image?: string | null;
+    name: string;
+};
+
+export default function UserDropdown({ image, name }: UserDropdownProps) {
+    return (
+        <div className="border-highlight flex select-none items-center gap-2 rounded-md bg-secondary px-3 py-2">
+            <Avatar className="h-6 w-6">
+                <AvatarImage src={image ?? "/logo.png"} />
+                <AvatarFallback>
+                    {name
+                        .split(" ")
+                        .map((name) => name[0])
+                        .join("") ?? "U"}
+                </AvatarFallback>
+            </Avatar>
+            <p className="text-sm font-medium">{name}</p>
+            <ChevronDown className="h-4 w-4 text-primary transition-all duration-300 ease-in-out group-data-[state=open]:rotate-180" />
+        </div>
+    );
+}
