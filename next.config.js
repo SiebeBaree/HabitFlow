b986c5d556d01596dcd14d16aba4915a78fdf48f
Siebe Baree <siebe.baree@outlook.com>
2024-04-02 11:03:41 +0200

    Added discord and google image urls to next config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,29 @@
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "cdn.discordapp.com",
+                port: "",
+                pathname: "/embed/avatars/**",
+            },
+            {
+                protocol: "https",
+                hostname: "cdn.discordapp.com",
+                port: "",
+                pathname: "/avatars/**",
+            },
+            {
+                protocol: "https",
+                hostname: "*.googleusercontent.com",
+                port: "",
+                pathname: "/**",
+            },
+        ],
+    },
+};
 
 export default config;
