01adabf627a83ba874162cece3d1b65d3ce6e5ed
Siebe Baree <siebe.baree@outlook.com>
2024-04-02 18:29:18 +0200

    Added extra utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,13 @@
+import type { Route } from "@/types";
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import {
+    LayoutDashboardIcon,
+    ArchiveIcon,
+    HomeIcon,
+    LineChartIcon,
+    SettingsIcon,
+} from "lucide-react";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function takeUniqueOrThrow<T extends any[]>(values: T): T[number] {
@@ -11,4 +19,46 @@ export function takeUniqueOrThrow<T extends any[]>(values: T): T[number] {
 
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
+}
+
+export function getDaysInMonth(date: Date): number {
+    return new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+}
+
+export function getFirstDayOfMonth(date: Date): Date {
+    return new Date(date.getFullYear(), date.getMonth(), 1);
+}
+
+export function getLastDayOfMonth(date: Date): Date {
+    return new Date(date.getFullYear(), date.getMonth() + 1, 0);
+}
+
+export function getDashboardRoutes(): Route[] {
+    return [
+        {
+            label: "Habit Tracker",
+            icon: LayoutDashboardIcon,
+            href: "/app",
+        },
+        {
+            label: "Habits",
+            icon: ArchiveIcon,
+            href: "/app/habits",
+        },
+        {
+            label: "Statistics",
+            icon: LineChartIcon,
+            href: "/app/statistics",
+        },
+        {
+            label: "Settings",
+            icon: SettingsIcon,
+            href: "/app/setttings",
+        },
+        {
+            label: "Home",
+            icon: HomeIcon,
+            href: "/",
+        },
+    ];
 }
