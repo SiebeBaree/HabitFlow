be113c9709fc5386f9e39a8772a05ad6ff30a302
Siebe Baree <siebe.baree@outlook.com>
2024-04-03 11:26:29 +0200

    Fixed id redirect

--- src/components/home/faq.tsx
+++ src/components/home/faq.tsx
@@ -8,39 +8,45 @@ import SectionWrapper from "@/components/home/section-wrapper";
 
 export default function FrequentlyAskedQuestions() {
     return (
-        <SectionWrapper className="mt-56 flex flex-col justify-between gap-12 lg:flex-row lg:gap-32">
-            <h2
-                className="mb-8 flex-1 text-center lg:mb-0 lg:text-start"
+        <SectionWrapper className="mt-28">
+            <div
                 id="faq"
+                className="flex w-full flex-col justify-between gap-12 pt-28 lg:flex-row lg:gap-32"
             >
-                Frequently Asked Questions
-            </h2>
-            <Accordion type="single" collapsible className="flex-grow">
-                <FAQItem
-                    value="habitflow-availability-mobile"
-                    question="Is HabitFlow available on mobile?"
-                >
-                    Yes. It adheres to the WAI-ARIA design pattern.
-                </FAQItem>
-                <FAQItem value="discounts" question="Can I get a discount?">
-                    Yes. It adheres to the WAI-ARIA design pattern.
-                </FAQItem>
-                <FAQItem
-                    value="implement-feature-x"
-                    question="Can you implement X feature?"
-                >
-                    Yes. It adheres to the WAI-ARIA design pattern.
-                </FAQItem>
-                <FAQItem value="data-security" question="Is my data secure?">
-                    Yes. It adheres to the WAI-ARIA design pattern.
-                </FAQItem>
-                <FAQItem
-                    value="community"
-                    question="Is there a community I can join?"
-                >
-                    Yes. It adheres to the WAI-ARIA design pattern.
-                </FAQItem>
-            </Accordion>
+                <h2 className="mb-8 flex-1 text-center lg:mb-0 lg:text-start">
+                    Frequently Asked Questions
+                </h2>
+
+                <Accordion type="single" collapsible className="flex-grow">
+                    <FAQItem
+                        value="habitflow-availability-mobile"
+                        question="Is HabitFlow available on mobile?"
+                    >
+                        Yes. It adheres to the WAI-ARIA design pattern.
+                    </FAQItem>
+                    <FAQItem value="discounts" question="Can I get a discount?">
+                        Yes. It adheres to the WAI-ARIA design pattern.
+                    </FAQItem>
+                    <FAQItem
+                        value="implement-feature-x"
+                        question="Can you implement X feature?"
+                    >
+                        Yes. It adheres to the WAI-ARIA design pattern.
+                    </FAQItem>
+                    <FAQItem
+                        value="data-security"
+                        question="Is my data secure?"
+                    >
+                        Yes. It adheres to the WAI-ARIA design pattern.
+                    </FAQItem>
+                    <FAQItem
+                        value="community"
+                        question="Is there a community I can join?"
+                    >
+                        Yes. It adheres to the WAI-ARIA design pattern.
+                    </FAQItem>
+                </Accordion>
+            </div>
         </SectionWrapper>
     );
 }
