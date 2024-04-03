0d9787e747e695e6e038eb1636a775edf73eaa40
Siebe Baree <siebe.baree@outlook.com>
2024-04-03 16:06:27 +0200

    Disabled PWA support in dev mode

--- next.config.js
+++ next.config.js
@@ -9,6 +9,7 @@ import nextPWA from "next-pwa";
 /** @type {import("next-pwa").WithPWA} */
 const withPWA = nextPWA({
     dest: "public",
+    disable: process.env.NODE_ENV === "development",
 });
 
 /** @type {import("next").NextConfig} */
