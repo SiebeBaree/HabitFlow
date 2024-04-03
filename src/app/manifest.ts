7830902a3f760fd56e501d3cc5afce7eca8c5073
Siebe Baree <siebe.baree@outlook.com>
2024-04-03 16:02:24 +0200

    Added more icons to manifest

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -4,7 +4,8 @@ export default function manifest(): MetadataRoute.Manifest {
     return {
         name: "HabitFlow",
         short_name: "HabitFlow",
-        description: "TODO",
+        description:
+            "Build habits, stop procrastinating, and improve your life.",
         start_url: "/",
         display: "standalone",
         background_color: "#fff",
@@ -12,8 +13,31 @@ export default function manifest(): MetadataRoute.Manifest {
         icons: [
             {
                 src: "/favicon.ico",
-                sizes: "any",
+                sizes: "48x48 32x32 24x24 16x16",
                 type: "image/x-icon",
+            },
+            {
+                src: "/android-chrome-192x192.png",
+                sizes: "192x192",
+                type: "image/png",
+                purpose: "any",
+            },
+            {
+                src: "/android-chrome-512x512.png",
+                sizes: "512x512",
+                type: "image/png",
+                purpose: "any",
+            },
+            {
+                src: "/android-chrome-512x512.png",
+                sizes: "512x512",
+                type: "image/png",
+                purpose: "maskable",
+            },
+            {
+                src: "/logo.png",
+                sizes: "2048x2048",
+                type: "image/png",
             },
         ],
     };
